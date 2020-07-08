7d348a96e4cca85833211549c20c2bbc0bdefe74
Takaaki Iwamoto <iwamoto.takaaki@gmail.com>
2020-07-08 19:22:08 +0900

    install something

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueComponentApi from '@vue/composition-api';
 import router from './router';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+Vue.use(VueComponentApi);
+
+library.add(fas);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
