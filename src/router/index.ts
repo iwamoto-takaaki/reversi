5b773c13a8afe521c2ac7dd691732c2da8f787bb
Takaaki Iwamoto <iwamoto.takaaki@gmail.com>
2020-07-08 20:19:02 +0900

    add Auth.vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,11 +13,13 @@ const routes: RouteConfig[] = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import('../views/About.vue'),
   },
+  {
+    path: '/auth',
+    name: 'Auth',
+    component: () => import('@/views/Auth.vue')
+  }
 ];
 
 const router = new VueRouter({
---
+++ src/views/Auth.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  #firebaseui-auth-container
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from '@vue/composition-api'
+import { authObject, ui } from '@/scripts/firebase'
+
+export default defineComponent({
+    setup(props, { root }) {
+        const from = () => {
+            if (root.$route.query.from) {
+                return root.$route.query.from as string
+            }
+            return '/'
+        }
+
+        onMounted(() => {
+            ui.start('#firebaseui-auth-container', {
+                signInOptions: [authObject.EmailAuthProvider.PROVIDER_ID],
+                signInSuccessUrl: from(),
+            })
+        })
+    }
+})
+</script>
