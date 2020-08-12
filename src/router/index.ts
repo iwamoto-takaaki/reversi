0ef462da343f461abb0fc0d6afe023b3aaa3ff27
Takaaki Iwamoto <iwamoto.takaaki@gmail.com>
2020-08-12 13:38:43 +0900

    change router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,29 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
-const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('@/views/Home.vue'),
-  },
-  {
-    path: '/auth',
-    name: 'Auth',
-    component: () => import('@/views/Auth.vue'),
-  },
-  {
-    path: '/gametables',
-    name: 'GameTables',
-    component: () => import('@/views/GameTables.vue'),
-  },
-  {
-    path: '/game/:gameTableId',
-    name: 'Game',
-    component: () => import('@/views/Game.vue'),
-  },
-];
-
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('@/views/Home.vue'),
+    },
+    {
+      path: '/auth',
+      name: 'Auth',
+      component: () => import('@/views/Auth.vue'),
+    },
+    {
+      path: '/gametables',
+      name: 'GameTables',
+      component: () => import('@/views/GameTables.vue'),
+    },
+    {
+      path: '/game/:gameTableId',
+      name: 'Game',
+      component: () => import('@/views/Game.vue'),
+    },
+  ],
 });
 
 export default router;
